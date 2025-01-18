13fad7cfbe08d63afe5807b623d7137d2172e345
mmAbdullahrana <ar292211@gmail.com>
2025-01-18 12:09:55 +0500

    Intial commit

---
+++ app/account/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Account',
+}
+
+export default function Page() {
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Abdullah
+      </h2>
+    </div>
+  );
+}
